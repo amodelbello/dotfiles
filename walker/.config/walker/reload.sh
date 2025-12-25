5426b46c027bd2b1ecb5bf66d8f1bc4f9950e489
Amo DelBello <amodelbello@protonmail.com>
2025-12-25 12:46:08 -0700

    Make elephant/walker better

---
+++ walker/.config/walker/reload.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+
+killall walker
+walker --gapplication-service
+
+
+yay -S elephant-bluetooth \
+elephant-archlinuxpkgs \
+elephant-calc \
+elephant-providerlist \
+elephant-websearch \
+elephant-desktopapplications \
+elephant-files \
+elephant-todo \
+elephant-runner \
+elephant-symbols \
+elephant-unicode \
+elephant-clipboard \
+elephant-menus
